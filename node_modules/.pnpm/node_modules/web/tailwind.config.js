39e9a5d20fd10b6fcd00e30f13826a4162c57f1f
Nathália Almeida Amorim <nathaliaalmeida@prefeitura.sp.gov.br>
2025-12-23 12:46:53 -0300

    style: adicionando as cores no arquivo de config do tailwind

--- node_modules/.pnpm/node_modules/web/tailwind.config.js
+++ node_modules/.pnpm/node_modules/web/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F07142',
+        secondary: '#12354D',
+
+        texts: {
+          primary: '#614B44',
+          'primary-dark': '#4A342D',
+          default: '#897872',
+        },
+      },
+    },
   },
   plugins: [],
 }
