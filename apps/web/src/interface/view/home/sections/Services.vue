9536b44de2a186628171ea448ce5d66f6c5c4f0b
Nathália Almeida Amorim <nathaliaalmeida@prefeitura.sp.gov.br>
2026-01-06 13:28:43 -0300

    style: adicionando imagens

--- apps/web/src/interface/view/home/sections/Services.vue
+++ apps/web/src/interface/view/home/sections/Services.vue
@@ -6,7 +6,7 @@
         <p class="w-[60%] font-medium text-texts-primary-dark/70">Aqui você encontra dog walkers e cuidadores verificados, escolhe quem combina com você e com seu pet, e pode entrar em contato direto, sem intermediários nem enrolação.</p>
        </div>
        <div class="mt-[6rem]">
-          <div class="bg-white h-auto w-[24rem] shadow-sm rounded">
+          <div class="bg-white h-auto w-[24rem] shadow-sm rounded flex flex-col items-center justify-center p-5">
             <div><!---icone--></div>
             <h2>Adestrador</h2>
             <p>adhwawid</p>
