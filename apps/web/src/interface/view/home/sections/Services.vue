d0d36f2bc1122c32cb697c885d7e6ecbed4b72ee
Nathália Almeida Amorim <nathaliaalmeida@prefeitura.sp.gov.br>
2026-01-06 12:57:47 -0300

    commit padrção atualização

--- apps/web/src/interface/view/home/sections/Services.vue
+++ apps/web/src/interface/view/home/sections/Services.vue
@@ -5,6 +5,14 @@
          <h1 class="text-4xl font-bold text-texts-primary-dark">Serviços oferecidos em nossa plataforma!</h1>
         <p class="w-[60%] font-medium text-texts-primary-dark/70">Aqui você encontra dog walkers e cuidadores verificados, escolhe quem combina com você e com seu pet, e pode entrar em contato direto, sem intermediários nem enrolação.</p>
        </div>
+       <div class="mt-[6rem]">
+          <div class="bg-white h-auto w-[24rem] shadow-sm rounded">
+            <div><!---icone--></div>
+            <h2>Adestrador</h2>
+            <p>adhwawid</p>
+            <button>Contratar</button>
+          </div>
+       </div>
        </div>
     </section>
 </template>
