1f1558100dae89325b9ca870a673b6f835c5f271
Nathália Almeida Amorim <nathaliaalmeida@prefeitura.sp.gov.br>
2026-01-07 11:52:01 -0300

    feat: sessão home ser colaborador

---
+++ apps/web/src/interface/view/home/sections/Collaborator.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <div class="w-full h-[500px] bg-secondary flex justify-between pt-36 px-20">
+            <div class="w-[50%] flex flex-col items-start justify-center gap-24">
+                <div class="text-white flex flex-col gap-10">
+                    <h1 class="text-4xl font-bold">
+                        Seja você tambem um colaborador!
+                    </h1>
+                    <p class="text-white/70">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, 
+                    </p>
+                </div>
+                <button class="w-[24rem] h-12 rounded-xl font-semibold bg-primary text-white hover:bg-primaryHover transition">Ser Colaborador</button>
+            </div>
+            <div class="w-[40%] h-auto flex items-end justify-end">
+                <img width="500" height="500" src="/assets/home/catHome.svg" alt="">
+            </div>
+        </div>
+    </section>
+</template>
--- node_modules/.pnpm/node_modules/web/src/interface/view/home/home.vue
+++ node_modules/.pnpm/node_modules/web/src/interface/view/home/home.vue
@@ -3,9 +3,11 @@
        <SearchMain />
        <VideoSection />
        <Services/>
+       <Collaborator/>
     </main>
 </template>
 <script  setup lang="ts">
+import Collaborator from './sections/Collaborator.vue';
 import SearchMain from './sections/SearchMain.vue';
 import Services from './sections/Services.vue';
 import VideoSection from './sections/VideoSection.vue';
