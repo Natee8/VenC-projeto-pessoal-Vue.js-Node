f275e91d1f90c8aad56386300a4cf194bcdabc25
Nathália Almeida Amorim <nathaliaalmeida@prefeitura.sp.gov.br>
2026-01-07 13:33:35 -0300

    build: adicionando nest no back-end

---
+++ apps/service/domain/dtos/auth.dto.ts
@@ -0,0 +1,13 @@
+import { UserRole } from "../../../../packages/types/userType";
+
+export interface LoginRequestDTO {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponseDTO {
+  token: string;
+  userId: string;
+  role: UserRole;
+  name: string;
+}
