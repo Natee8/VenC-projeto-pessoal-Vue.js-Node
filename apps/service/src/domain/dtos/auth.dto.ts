a62984d88e17075db11f7a9983c9bedd84560fa1
Nathália Almeida Amorim <nathaliaalmeida@prefeitura.sp.gov.br>
2026-01-08 08:36:32 -0300

    commit

--- apps/service/src/domain/dtos/auth.dto.ts
+++ apps/service/src/domain/dtos/auth.dto.ts
@@ -1,4 +1,4 @@
-import { UserRole } from "../../../../packages/types/userType";
+import { UserRole } from "../../../../../packages/types/userType";
 
 export interface LoginRequestDTO {
   email: string;
